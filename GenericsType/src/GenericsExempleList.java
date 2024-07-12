d7145ed93d8b637c0aaadc5fb27953feb4bb73fc
Bradin <danbleeding123@gmail.com>
2024-07-12 11:00:15 -0300

    Add files via upload

---
+++ GenericsType/src/GenericsExempleList.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class GenericsExempleList {
+    public static void main(String[] args) {
+        List listaSemGenerics = new ArrayList();
+        listaSemGenerics.add("Elemento 1");
+        listaSemGenerics.add(10);
+
+        List<String> listaGenerics = new ArrayList<>();
+        listaGenerics.add("Elemento 1");
+        listaGenerics.add("Elemento 2");
+
+        for (String elemento : listaGenerics) {
+            System.out.println(elemento);
+        }
+        for (Object elemento : listaSemGenerics) {
+            String str = (String) elemento;
+            System.out.println(str);
+        }
+        }
+}
